d600feb9f66ca4e9da002f14a43d0c3bcba991c3
martalmeida03 <107552754+martalmeida03@users.noreply.github.com>
2024-05-18 19:14:19 +0100

    nova atualização

--- js/script.js
+++ js/script.js
@@ -74,3 +74,23 @@ const init = () => {
 
 window.onload = init;
 
+document.getElementById('passwordForm').addEventListener('submit', function(event) {
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+
+    if (password1 !== password2) {
+        alert('As senhas não são iguais.');
+        event.preventDefault();
+    }
+});
+
+document.getElementById('passwordForm').addEventListener('submit', function(event) {
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+
+    if (password1 !== password2) {
+        alert('As senhas não são iguais.');
+        event.preventDefault();
+    }
+});
+
